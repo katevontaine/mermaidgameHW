e8c3327c29eed175cc4b3cba070b70de244a6ea9
Kate Wilson <KateWilson@Kates-MacBook-Pro.local>
2015-10-28 13:14:09 -0400

    beign

---
+++ game.js
@@ -0,0 +1,14 @@
+function ship(){
+  this.name = function(){
+    return "Santa Maria";
+  }
+    this.size = function(){
+      return "Large"
+  }
+}
+var Pinta = new ship {name:"Pinta", size:"Medium"}
+var Nina = new ship{name:'Nina', size:'Small'}
+
+function weather(){
+  
+}
